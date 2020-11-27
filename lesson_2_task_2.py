9744f3816e664b6f63bf1af9eab12b61f04b914b
Vadim <hyfrvadim@gmail.com>
2020-11-27 17:01:43 +0700

    -

--- lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-first = input('Введите данные для списка:')
-first_list = list(first)
-print(first_list)
-new_list = [i for i in first_list]
-for i in range(1, len(new_list), 2):
-    new_list[i - 1], new_list[i] = new_list[i], new_list[i - 1]
-print(new_list)
-
